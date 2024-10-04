451ab82d5201c1ca4e21a075ec3fac542cb03a96
JoseVinGarcia <josevingarcia2020@gmail.com>
2024-10-03 21:36:38 -0300

    Adendo final

--- Atividade/atividade2_a8.py
+++ Atividade/atividade2_a8.py
@@ -8,17 +8,17 @@ limpa_tela()
 # ATIVIDADE 2
 
 def calcula_multa(pp):
-    m = (pp - 100) * 4
+    m = (pp - LIMITE) * MULTA
     return m
 
-
-peso_padrao = 100.00
+MULTA = 4
+LIMITE = 100.00
 
 print("-"*30)
 
 peso_peixe = float(input("Digite o peso do peixe pescado: "))
 
-if peso_peixe > peso_padrao:
+if peso_peixe > LIMITE:
     valor_multa = calcula_multa(peso_peixe)
     print(f"Sua multa será de R$ {valor_multa}.")
 else:
